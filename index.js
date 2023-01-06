6769c814c44264e1bd94647493a17647bc5036a5
CharlesAD <c.acquahdavis@gamil.com>
2023-01-06 18:50:24 +0000

    Create a function to shuffling the deck

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function createDeck(){
             deck.push(values[j] + "-" + types[i]);
         }
     }
+// console.log(deck);
+}
+function shuffleDeck() {
+    for(i = 0; i < 1000; i++) {
+        let location1 = Math.floor((Math.random() * deck.length));
+        let location2 = Math.floor((Math.random() * deck.length));
+        let temp = deck[location1];
+
+        deck[location1] = deck[location2];
+        deck[location2] = temp;
+    }
+    console.log(deck); 
+}
